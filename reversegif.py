ba4f2e49f19f5ee2a7f7263eaf45e31ce6561d08
aldilaff <aldilaff@gmail.com>
2016-01-22 20:28:46 -0600

    added try except for imgur. Also fixes #2

--- reversegif.py
+++ reversegif.py
@@ -5,29 +5,46 @@ from moviepy.video.fx.time_mirror import time_mirror
 from numpy import fliplr
 from config import *
 from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 from os import remove
 import praw
 import time
 import requests
 import json
-
+import pprint
 
 def reverseGif(gifUrl):
 	request = requests.get(gifUrl)
-	chunk_size = 10
+	chunk_size = 1024
 	with open('gif.gif', 'wb') as fd:
 		for chunk in request.iter_content(chunk_size):
 			fd.write(chunk)
 	client = ImgurClient(imgur_client_id, imgur_client_secret)
-	gifToBeReversed = 'gif.gif' #can be either gif or url to a gif
+	gifToBeReversed = "gif.gif" #can be either gif or url to a gif
 	reversedGifFileName = 'reversed.gif'
 	print('getting gif')
 	clip = VideoFileClip(gifToBeReversed)
+	# print(clip.fps)
+	# print(clip.duration)
+	# print(ffmpeg_parse_infos(gifToBeReversed, True, True))
+	# clip.duration = 15
+	# print(clip.duration)
+	# i=0
+	# for frame in clip.iter_frames():
+	# 	print(i)
+	# 	i+=1
+	# pp = pprint.PrettyPrinter(indent=4)
+	# pp.pprint(clip)
 	print('reversing gif')
 	clip = clip.fx(time_mirror)
 	clip.write_gif(reversedGifFileName)
-	print("Uploading image... ")
-	imageURL = client.upload_from_path(reversedGifFileName, config=None, anon=False)
+	imageURL = None
+	try:
+		print("Uploading image... ")
+		imageURL = client.upload_from_path(reversedGifFileName, config=None, anon=False)
+	except ImgurClientError as e:
+	    print(e.error_message)
+	    print(e.status_code)
 	print("Done")
 	print()
 	print("Image was posted! Go check your images you sexy beast!")
@@ -42,8 +59,8 @@ def main():
     r = praw.Reddit(user_agent='Reverse gif bot by /u/aldilaff')
     r.login(reddit_username, reddit_password)
 	# r.send_message('aldilaff', 'Test1', 'You are awesome!')
-    # print('calling reverseGif')
-    # reverseGif('http://i.imgur.com/Duqccay.gif')
+    # print('calling reverseGif()')
+    # reverseGif('hlN3NZZ.gif')
     while True:
 	    for msg in r.get_unread(limit=None):
 	    	# if msg.is_root:
