4bf7b3da9b7096d89eb936694aa592770828cc27
aldilaff <aldilaff@gmail.com>
2016-02-01 16:37:40 -0600

    updated imports and cleaned up code a bit

--- reversegif.py
+++ reversegif.py
@@ -1,12 +1,6 @@
 # help from: https://www.reddit.com/r/Python/comments/2angq5/ffmpeg_how_to_get_frames_from_gifvideo/
 
-from moviepy.editor import VideoFileClip
-from moviepy.video.fx.time_mirror import time_mirror
-from numpy import fliplr
 from config import *
-from imgurpython import ImgurClient
-from imgurpython.helpers.error import ImgurClientError
-from os import remove
 import praw
 import time
 import requests
@@ -26,20 +20,15 @@ def reverseGif(gifUrl):
 	    print(e.status_code)
 	print("Done")
 	print()
-	print("Image was posted! Go check your images you sexy beast!")
-	# print("You can find it here: {0}".format(imageURL['link']))
 	print("You can find it here: {0}".format(imageURL))
 	return imageURL["gfyName"]
 
 def main():
     r = praw.Reddit(user_agent='Reverse gif bot by /u/aldilaff')
     r.login(reddit_username, reddit_password)
-	# r.send_message('aldilaff', 'Test1', 'You are awesome!')
 
     while True:
 	    for msg in r.get_unread(limit=None):
-	    	# if msg.is_root:
-	    	# 	print('comment is a root')
 	    	time.sleep(2)
 	    	print('Message: %s' % msg)
 	    	print('Message link: %s' % msg.permalink)
@@ -54,7 +43,6 @@ def main():
 	    	time.sleep(2)
 	    	msg.mark_as_read()
 	    	print('Marked message as read')
-	    #reverseGif('http://i.imgur.com/Z5mi7mI.gif')
 	    print('Sleeping for 3 seconds...')
 	    time.sleep(3)
 if __name__ == "__main__":
