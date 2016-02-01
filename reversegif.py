a08aaaeef8b52bf578787db0a036df672152d1dd
aldilaff <aldilaff@gmail.com>
2016-02-01 00:50:04 -0600

    updated requirements.txt and reversegif.py. reversegif.py now uses gfycat.com

--- reversegif.py
+++ reversegif.py
@@ -12,55 +12,30 @@ import time
 import requests
 import json
 import pprint
+from gfycat.client import GfycatClient
+from gfycat.error import GfycatClientError
+
 
 def reverseGif(gifUrl):
-	request = requests.get(gifUrl)
-	chunk_size = 1024
-	with open('gif.gif', 'wb') as fd:
-		for chunk in request.iter_content(chunk_size):
-			fd.write(chunk)
-	client = ImgurClient(imgur_client_id, imgur_client_secret)
-	gifToBeReversed = "gif.gif" #can be either gif or url to a gif
-	reversedGifFileName = 'reversed.gif'
-	print('getting gif')
-	clip = VideoFileClip(gifToBeReversed)
-	# print(clip.fps)
-	# print(clip.duration)
-	# print(ffmpeg_parse_infos(gifToBeReversed, True, True))
-	# clip.duration = 15
-	# print(clip.duration)
-	# i=0
-	# for frame in clip.iter_frames():
-	# 	print(i)
-	# 	i+=1
-	# pp = pprint.PrettyPrinter(indent=4)
-	# pp.pprint(clip)
-	print('reversing gif')
-	clip = clip.fx(time_mirror)
-	clip.write_gif(reversedGifFileName)
 	imageURL = None
+	client = GfycatClient()
 	try:
-		print("Uploading image... ")
-		imageURL = client.upload_from_path(reversedGifFileName, config=None, anon=False)
-	except ImgurClientError as e:
+		imageURL= client.upload_from_url(gifUrl)
+	except GfycatClientError as e:
 	    print(e.error_message)
 	    print(e.status_code)
 	print("Done")
 	print()
 	print("Image was posted! Go check your images you sexy beast!")
-	print("You can find it here: {0}".format(imageURL['link']))
-	print("Deleting gifs")
-	remove('gif.gif')
-	remove(reversedGifFileName)
-	print("gif deleted")
-	return imageURL['link']
+	# print("You can find it here: {0}".format(imageURL['link']))
+	print("You can find it here: {0}".format(imageURL))
+	return imageURL["gfyName"]
 
 def main():
     r = praw.Reddit(user_agent='Reverse gif bot by /u/aldilaff')
     r.login(reddit_username, reddit_password)
 	# r.send_message('aldilaff', 'Test1', 'You are awesome!')
-    # print('calling reverseGif()')
-    # reverseGif('hlN3NZZ.gif')
+
     while True:
 	    for msg in r.get_unread(limit=None):
 	    	# if msg.is_root:
@@ -73,7 +48,8 @@ def main():
 	    	print(submission.url)
 	    	reversedGifUrl = reverseGif(submission.url)
 	    	time.sleep(2)
-	    	msg.reply('reversed %s' % reversedGifUrl) #reply to comment
+	    	print('reversed https://gfycat.com/%s#?direction=reverse' % reversedGifUrl)
+	    	msg.reply('reversed https://gfycat.com/%s#?direction=reverse' % reversedGifUrl) #reply to comment
 	    	print('Replied to message')
 	    	time.sleep(2)
 	    	msg.mark_as_read()
